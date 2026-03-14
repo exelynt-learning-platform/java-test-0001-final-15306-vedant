9ff91e413da3117ccaffdfb163f68e5f013fd8c4
VedantK08 <vedant.kulkarni777@gmail.com>
2026-03-14 15:56:11 +0530

    Solved Java Question 1

---
+++ Java0001Question.java
@@ -0,0 +1,36 @@
+package com.java.string25;
+
+public class Java0001Question {
+
+	public static void main(String[] args) {
+		int n = 5;
+		// Upper Half
+		for (int i = 1; i <= n; i++) {
+			for (int j = i; j < n; j++) {
+				System.out.print(" ");
+			}
+			System.out.print("*");
+			if (i > 1) {
+				for (int j = 1; j <= (2 * i - 3); j++) {
+					System.out.print(" ");
+				}
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		// Lower Half
+		for (int i = n - 1; i >= 1; i--) {
+			for (int j = n; j > i; j--) {
+				System.out.print(" ");
+			}
+			System.out.print("*");
+			if (i > 1) {
+				for (int j = 1; j <= (2 * i - 3); j++) {
+					System.out.print(" ");
+				}
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
